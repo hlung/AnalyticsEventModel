fc054364ec8e062dafaf818e6d61f1cc8854995f
Thongchai Kolyutsakul <thongchaikol@gmail.com>
2022-01-22 10:01:30 +0700

    Declare enum in snake case

--- Sources/Analytics/Event.swift
+++ Sources/Analytics/Event.swift
@@ -20,8 +20,11 @@ class Event {
 
   var dictionary: [String: String] = [:]
 
-  init(_ name: Name) {
+  init(_ name: Name, _ parameters: Parameter...) {
     dictionary["name"] = name.rawValue
+    for parameter in parameters {
+      dictionary[parameter.key] = parameter.value
+    }
   }
 
   func add(_ parameter: Parameter) {
@@ -47,19 +50,56 @@ extension Event {
 
   enum Parameter {
     case what(What)
+    case page(Page)
+    case page_id(String)
 
     enum What: String, HasStringRawValue {
-      case button
+      case download_button
+    }
+
+    enum Page: String, HasStringRawValue {
+      case home
     }
 
     var key: String {
+      // Option 1
+      // We could use Mirror here. But I think having snake case strings improves searchability.
+//      switch self {
+//      case .what: return "what"
+//      case .page: return "page"
+//      }
+
+      // Option 2
+      // Declare enum in snake case to improve searchability and use Mirror :P
       let mirror = Mirror(reflecting: self)
       return mirror.children.first?.label ?? ""
     }
 
     var value: String {
+      // Option 1
+      // Use snake case values defined in enum rawValue
+//      switch self {
+//      case .what(let value): return value.rawValue
+//      case .page(let value): return value.rawValue
+//      }
+
+      // Option 2
+      // Declare enum in snake case to improve searchability and use Mirror :P
       let mirror = Mirror(reflecting: self)
-      return (mirror.children.first?.value as? HasStringRawValue)?.rawValue ?? ""
+      let value: Any? = mirror.children.first?.value
+      if let string = (value as? HasStringRawValue)?.rawValue {
+        return string
+      }
+      else if let string = value as? String {
+        return string
+      }
+      else {
+        #if DEBUG
+        fatalError("value is invalid")
+        #else
+        return ""
+        #endif
+      }
     }
   }
 
--- Tests/AnalyticsTests/AnalyticsTests.swift
+++ Tests/AnalyticsTests/AnalyticsTests.swift
@@ -3,12 +3,14 @@ import XCTest
 
 final class AnalyticsTests: XCTestCase {
   func testExample() throws {
-    let event = Event(.click)
-    event.add(.what(.button))
+    let event = Event(.click, .page(.home), .page_id("123"))
+    event.add(.what(.download_button))
 
     XCTAssertEqual(event.dictionary, [
       "name": "click",
-      "what": "button"
+      "what": "download_button",
+      "page": "home",
+      "page_id": "123"
     ])
   }
 }
